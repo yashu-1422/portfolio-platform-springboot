67c4cdf253904147d0eaf162659886e800ca0751
yashu-1422 <yashauti5@gmail.com>
2026-01-31 19:38:07 +0530

    Initial commit

---
+++ portfolio-platform/src/main/java/com/portfolio/portfolio_platform/Entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.portfolio.portfolio_platform.Entity;
+
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
